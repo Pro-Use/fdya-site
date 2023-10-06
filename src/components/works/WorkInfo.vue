87dc28465be3409a1ac77c980bc9c28b30076c26
pro-use <robertprouse@gmail.com>
2023-10-06 15:33:27 +0100

    Add work view and work info

---
+++ src/components/works/WorkInfo.vue
@@ -0,0 +1,25 @@
+<template>
+	<div></div>
+</template>
+<script setup>
+	import { defineProps, ref, computed } from 'vue'
+	import { useStateStore } from '../../stores/state'
+
+	const state = useStateStore()
+	const props = defineProps(['work'])
+	console.log(props.work)
+
+	const work_info = computed(() => {
+		const filter_work = state.worksInfo.filter((work_obj) => work_obj.name == props.work)
+		console.log(filter_work)
+		if (filter_work.length != 0) {
+			return filter_work[0]
+		} else {
+			return null
+		}
+	})
+	
+</script>
+<style scoped>
+	
+</style>
--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -1,9 +1,27 @@
 <template>
-	<div></div>
+	<WorkInfo :work="props.work" />
 </template>
 
 <script setup>
-	
+	import { ref, defineProps, onMounted } from 'vue';
+	import { useRoute } from 'vue-router'
+	import { useStateStore } from '../stores/state'
+	import WorkInfo from '../components/works/WorkInfo.vue'
+
+	const state = useStateStore()
+	const props = defineProps(['langauge', 'work'])
+	const route = useRoute()
+
+	console.log(props)
+
+	// Set language if direct navigation
+	if (state.siteLang.value != props.langauge){
+		store.$patch({siteLang: props.language})
+	}
+
+
+
+
 </script>
 
 <style scoped>
