b934fc9615b591a2799e089aee8c3e41d2bcadac
pro-use <robertprouse@gmail.com>
2023-10-12 14:29:39 +0100

    video selection and path logic

--- src/components/works/VideoComponent.vue
+++ src/components/works/VideoComponent.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <video ref="video" class="w-screen h-screen absolute top-0 left-0 object-cover z-40" src="../../assets/test.mp4" autoplay>
+    <video ref="video" class="w-screen h-screen absolute top-0 left-0 object-cover z-40" :src="video" autoplay>
 
     </video>
 
@@ -11,18 +11,11 @@
 	import { defineProps, ref, computed } from 'vue'
 	import { useStateStore } from '../../stores/state'
 
-	const state = useStateStore()
-	const props = defineProps(['work'])
-	console.log(props.work)
-
-	const work_info = computed(() => {
-		const filter_work = state.worksInfo.filter((work_obj) => work_obj.slug == props.work)
-		console.log(filter_work)
-		if (filter_work.length != 0) {
-			return filter_work[0]
-		} else {
-			return null
-		}
+	const api_base =  import.meta.env.VITE_API_BASE
+	const props = defineProps(['video_file'])
+	console.log(props.video_file)
+	const video = computed(() => {
+		return `${api_base}assets/${props.video_file}`
 	})
 	
 </script>
