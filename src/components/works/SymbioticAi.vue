b2c6c975debefaf592ea6711a27e17b5cae5ea71
pro-use <robertprouse@gmail.com>
2023-10-12 14:33:59 +0100

    Add symbiotic AI

---
+++ src/components/works/SymbioticAi.vue
@@ -0,0 +1,20 @@
+<template>
+	<VideoComponent v-if="videos.length" :video_file="videos[0]"></VideoComponent>
+</template>
+<script setup>
+	import VideoComponent from './VideoComponent.vue'
+	import { defineProps, computed } from 'vue';
+	import { useStateStore } from '../../stores/state'
+	const state = useStateStore()
+
+	const props = defineProps(['work'])
+
+	const videos = computed(() => {
+    let filter_work = state.worksInfo.filter((work_obj) => work_obj.slug == props.work)
+    if (filter_work.length === 1 && filter_work[0].videos.length > 0){
+        return filter_work[0].videos.map((video_info) => video_info.directus_files_id)
+    } else {
+        return []
+    }
+})
+</script>
