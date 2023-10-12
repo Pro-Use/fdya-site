2bac4683e0df6508392bb1569aa341f8ec90c228
pro-use <robertprouse@gmail.com>
2023-10-12 14:29:24 +0100

    CL video

--- src/components/works/CrossLucid.vue
+++ src/components/works/CrossLucid.vue
@@ -1,12 +1,27 @@
 <template>
-    
+    <VideoComponent v-if="videos.length" :video_file="videos[0]"></VideoComponent>
 </template>
 
 <script setup>
+import { ref, computed, defineProps } from 'vue';
+import { useStateStore } from '../../stores/state'
 import { useClStore } from '../../stores/CrossLucid'
+import VideoComponent from './VideoComponent.vue'
 import axios from 'axios';
 
+
 const cl_store = useClStore()
+const state = useStateStore()
+const props = defineProps(['work'])
+
+const videos = computed(() => {
+    let filter_work = state.worksInfo.filter((work_obj) => work_obj.slug == props.work)
+    if (filter_work.length === 1 && filter_work[0].videos.length > 0){
+        return filter_work[0].videos.map((video_info) => video_info.directus_files_id)
+    } else {
+        return []
+    }
+})
 
 const selectScene = async () => {
     const now = new Date()
