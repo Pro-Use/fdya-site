89a1a952b08337106834930e8e09fbefa81379f0
merrelltom <merrelltom@gmail.com>
2023-11-02 13:02:55 +0000

    Style Access Work View

--- src/views/AccessWorkView.vue
+++ src/views/AccessWorkView.vue
@@ -1,31 +1,46 @@
 <template>
-	<main id="main" 
-	  	class="fixed w-screen h-screen bg-black z-50 top-0 left-0 overflow-y-auto text-white">
-		<div v-if="access_work_info.work">
-			<h1 class="">
-				<i>{{ work_info.title }}</i>
-				<br>{{ work_info.artist }}
-			</h1>
+    <main id="main" 
+		class="fixed w-screen h-screen top-0 left-0 overflow-y-auto bg-black-40 backdrop-blur-3xl text-white z-50 font-DMregular leading-tighterer">
+        <div class="grid-cols-modal lg:grid-cols-modal-xl grid gap-1">
+            <div class="modal-body grid grid-cols-2 gap-16  p-2 lg:p-4 max-w-[1200px]">
+                <div class="col-span-2">
+					<div v-if="access_work_info.work">
+						<h1 class="pb-8 text-lg xl:text-2xl uppercase">
+							<i>{{ work_info.title }}</i>
+							<br>{{ work_info.artist }}
+						</h1>
+					</div>
+					<h1 class="pb-8 text-lg xl:text-2xl uppercase" v-else>{{access_work_info.title}}</h1>
+				</div>
+				<div v-if="work_info" class="col-span-2">
+					<h2 class="pb-8 text-lg xl:text-2xl uppercase">
+						<div v-html="work_info.listing"></div>
+					</h2>
+					<div class="text-base xl:text-lg rte" v-html="work_info.description"></div>
+				</div>   
+				<figure v-if="audio_file" class="col-span-2">
+					<figcaption class="sr-only">{{access_work_info.accessible_description}}:</figcaption>
+					<audio controls :src="audio_file">
+					</audio>
+				</figure>
+			</div>
+			<div class="close-button">
+                <router-link class="sticky p-2 lg:p-4 top-0 text-lg xl:text-2xl text-white text-right block w-full hover:text-yellow" to="/">
+                    <span class="sr-only">{{state.getTrans('back-to-home-page')}}</span>
+                    <span aria-hidden="true">{{state.getTrans('close').toUpperCase()}}</span>
+                </router-link>
+            </div>
 		</div>
-		<div v-else>{{access_work_info.title}}</div>
-		<div v-if="work_info" class="grid grid-cols-2 gap-16 p-2 lg:p-4  max-w-[1200px]">
-	        <div class="col-span-2">
-				<h2 class="pb-8 text-lg xl:text-2xl uppercase">
-					<div v-html="work_info.listing"></div>
-				</h2>
-				<div class="text-base xl:text-lg rte" v-html="work_info.description"></div>
-	        </div>   
-	    </div>
-	    <figure v-if="audio_file">
-	        <figcaption class="sr-only">{{access_work_info.accessible_description}}:</figcaption>
-	        <audio controls autoplay :src="audio_file">
-	        </audio>
-	    </figure>
 	</main>
+
+
+
+
+
 </template>
 
 <script setup>
-	import { ref, computed } from 'vue'
+	import { ref, computed, onMounted } from 'vue'
 	import { useStateStore } from '../stores/state'
 
 	const state = useStateStore()
@@ -57,6 +72,12 @@
 		} else {
 			return null
 		}
+	})
+
+
+	onMounted(() => {
+		state.mainTransition = 'zoom-fade'
+		state.interfaceVisible = false
 	})
 
 </script>
