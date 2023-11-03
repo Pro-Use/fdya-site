3277ada19cf4f536104abde78a47a41e040e2185
pro-use <robertprouse@gmail.com>
2023-11-03 18:02:05 +0000

    play button when paused

--- src/components/works/VideoComponent.vue
+++ src/components/works/VideoComponent.vue
@@ -1,8 +1,13 @@
 <template>
-
-    <video ref="video" class="w-screen h-screen absolute top-0 left-0 object-contain z-40 bg-black" :src="video" autoplay playsinline="true">
-
-    </video>
+	<div class="h-full">
+	    <video 
+	    	ref="video" class="w-screen h-screen absolute top-0 left-0 object-contain z-40 bg-black" 
+	    	:src="video_file" autoplay playsinline="true">
+	    </video>
+	    <div v-if="is_paused" @click="video.play" class="h-full grid place-items-center cursor-pointer">
+			<PlayButton />
+		</div>
+	</div>
 
 
 </template>
@@ -10,18 +15,32 @@
 <script setup>
 	import { defineProps, ref, computed, onMounted, onUnmounted } from 'vue'
 	import { useStateStore } from '../../stores/state'
+	import PlayButton from '../../components/icons/PlayButton.vue'
 
 	const state = useStateStore()
 	const api_base =  import.meta.env.VITE_API_BASE
 	const props = defineProps(['video_file'])
-	console.log(props.video_file)
-	const video = computed(() => {
+	const video = ref(null)
+	const is_paused = ref(false)
+
+	const video_file = computed(() => {
 		return `${api_base}assets/${props.video_file}`
 	})
 
-
     onMounted( () => {
     	state.screensaver_disabled = true
+    	video.value.onplaying = () => {
+    		is_paused.value = false
+    	}
+    	video.value.onpause = () => {
+    		is_paused.value = true
+    	}
+
+    	setTimeout(() => {
+    		if (video.value.paused){
+    			is_paused.value = true
+    		}
+    	}, 200)
     })
 
     onUnmounted( () => {
