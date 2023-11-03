b5b7b13e1557e159c08bdf5ad18eb36803fc02ca
pro-use <robertprouse@gmail.com>
2023-11-03 18:02:46 +0000

    play button

---
+++ src/components/icons/PlayButton.vue
@@ -0,0 +1,12 @@
+<template>
+	<svg width="68" height="78" viewBox="0 0 68 78" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M68 39L0.5 77.9712L0.500003 0.0288696L68 39Z" fill="white"/>
+	</svg>
+</template>
+
+<style scoped lang="scss">
+	svg {
+        position: relative;
+ 		z-index: 40;
+    }
+</style>
