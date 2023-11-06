ce64d40ef580d9108eabddbad52ea6ac49e6e9bc
pro-use <robertprouse@gmail.com>
2023-11-06 15:36:27 +0000

    add control to audio guide

--- src/components/works/AudioGuide.vue
+++ src/components/works/AudioGuide.vue
@@ -1,8 +1,11 @@
 <template>
 	<div class="pb-8" aria-hidden="true">
 		<button @click="play_toggle()" class="flex">
-			<span>
+			<span v-if="!is_playing">
 				<AudioIconSmall></AudioIconSmall>
+			</span>
+			<span v-else>
+				<StopIconSmall />
 			</span>
 			<span class="text-xs row-start-1 pl-2 uppercase">{{access_work_info.accessible_description}}</span>
 			<audio ref="player" v-show="false"></audio>
@@ -11,14 +14,17 @@
 	
 </template>
 <script setup>
-	import { ref, computed, onMounted } from 'vue'
+	import { ref, computed, onMounted, inject } from 'vue'
 	import { useStateStore } from '../../stores/state'
 	import AudioIconSmall from '../icons/AudioIconSmall.vue';
+	import StopIconSmall from '../icons/StopIconSmall.vue'
 
 	const state = useStateStore()
 	const props = defineProps(['work'])
 	const api_base =  import.meta.env.VITE_API_BASE
 	const player = ref(null)
+	const is_playing = ref(false)
+	const v_player = inject('player')
 
 	const access_work_info = computed(() => {
 		const filter_work = state.accessInfoTranslated.filter((work_obj) => work_obj.slug == props.work)
@@ -42,14 +48,18 @@
 		console.log(player.value)
 		if (!player.value?.paused){
 			player.value.pause()
+			is_playing.value = false
 		} else if (audio_file.value !== null && player.value?.paused){
 			player.value.src = audio_file.value
 			player.value.play()
+			is_playing.value = true
+			if (v_player.value){
+				v_player.value.pause()
+			}
 		}
 	}
 
 	onMounted(() => {
-		// player.value = document.createElement('audio');
 	})
 	
 </script>
