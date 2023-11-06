7bfcd3f9b55f361be10f29b14400e2838dddea55
pro-use <robertprouse@gmail.com>
2023-11-06 15:38:16 +0000

    stop icon

---
+++ src/components/icons/StopIconSmall.vue
@@ -0,0 +1,15 @@
+<template>
+    <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M0 0H20V20H0V0Z" fill="white"/>
+	</svg>
+</template>
+<style scoped lang="scss">
+	
+	button:hover{
+		color:#B8F33A;
+		path{
+			fill: #B8F33A!important
+		}
+	}
+	
+</style>
