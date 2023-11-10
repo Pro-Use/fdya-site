2c2baf77c9da99e1ab9b34512dcc4f9dde845d16
Pro-Use <robertprouse@gmail.com>
2023-11-10 09:35:57 +0000

    Add pause button

---
+++ src/components/icons/PauseButton.vue
@@ -0,0 +1,10 @@
+<template>
+	<svg height="32px" viewBox="0 0 32 32" width="32px" xmlns="http://www.w3.org/2000/svg"><g id="background"><rect fill="none" height="32" width="32"/></g><g id="pause"><g><rect height="24" width="8" x="20" y="4" fill="white"/><rect height="24" width="8" x="4" y="4" fill="white"/></g></g></svg>
+</template>
+
+<style scoped lang="scss">
+	svg {
+        position: relative;
+ 		z-index: 40;
+    }
+</style>
